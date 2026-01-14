75b4409a0f09ded7cd7b9c44083ea57687daf73e
sreebhargavreddy-123 <sreebhargavreddy@gmail.com>
2026-01-14 16:23:09 +0530

    Day 2 updates

--- Day 2/Enumerator.py
+++ Day 2/Enumerator.py
@@ -1,3 +1,15 @@
 fruits=['apple','orange','banana']
 for index,value in enumerate(fruits):
     print(index,value)
+
+from enum import Enum
+
+
+class color(Enum):
+    RED=1
+    GREEN=2
+    BLUE=3
+
+print(color.RED.value)
+print(color.GREEN.value)
+print(color.BLUE.value)
