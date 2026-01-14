88da772978181584d299e4117693fca6c1e7e992
sreebhargavreddy-123 <sreebhargavreddy@gmail.com>
2026-01-14 10:16:00 +0530

    Day 2 updates

--- Day 2/Functions.py
+++ Day 2/Functions.py
@@ -6,3 +6,22 @@ def sub(a,b):
 
 add(100,500)
 print("Sub :",sub(10,5))
+
+
+def hello(greeting='Hello', name='world'):
+    print('%s, %s!' % (greeting, name))
+
+hello()
+hello('Greetings')
+hello('Greetings','Bhargav')
+
+
+def print_params(*params):
+    print(params)
+print_params('Testing')
+print_params(1, 2, 3,4)
+
+
+def print_params_1(**params):
+    print(params)
+print_params_1(x=1, y=2, z=3)
