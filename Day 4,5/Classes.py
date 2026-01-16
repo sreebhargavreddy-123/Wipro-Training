35a62a062efa97bb114f2974bb34e7534a7e9a1b
sreebhargavreddy-123 <sreebhargavreddy@gmail.com>
2026-01-16 10:00:55 +0530

    Day 3 updates

---
+++ Day 4,5/Classes.py
@@ -0,0 +1,13 @@
+class student:
+    def display(self):
+        print("This is student class")
+
+s1=student()
+s1.display()
+
+class calculator:
+    def add(self,a,b):
+        print(a+b)
+
+c = calculator()
+c.add(100,300)
