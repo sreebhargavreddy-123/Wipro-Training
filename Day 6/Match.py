800585a60695a4df87d4b724599fb66c87b53cde
sreebhargavreddy-123 <sreebhargavreddy@gmail.com>
2026-01-20 10:07:09 +0530

    Day 6 updates

--- Day 6/Match.py
+++ Day 6/Match.py
@@ -1,8 +1,11 @@
 import re
 
 text = "Python is fun"
-result = re.match("Python", text)
+result = re.match("Java", text)
 if result:
     print("match found")
 else:
-    print("no match found")
+    print("match not found")
+
+searchresult = re.search("Python", text)
+print(searchresult.group())
